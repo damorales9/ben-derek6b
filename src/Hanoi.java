3ba2ccb7d016e7de4f6127407a150965001c52bd
bjpazienza <pazeinza.log@gmail.com>
2018-10-09 13:28:54 -0400

    tried iterative

--- src/Hanoi.java
+++ src/Hanoi.java
@@ -12,3 +12,35 @@ public class Hanoi
 			hanoi(aux, dest, home, n-1);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public void solveIterative(int from, int to, int other, int n)
+{
+	Stack<Integer> from, to, other;
+	from = new Stack<Integer>();
+	to = new Stack<Integer>();
+	other = new Stack<Integer>();
+
+	for(int i = n; i > 0; i--)
+	{
+		from.push(i);
+	}
+
+	while(!from.isEmpty)
+	{
+		move(from, to, other, from.pop);
+	
+
+	from.push 
